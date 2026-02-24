790b692969e73b23d8044dfaed9eff3e3b06d17e
jadc <chehimj@gmail.com>
2026-02-24 15:20:54 -0700

    Update flake to use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
-    description = "devenv";
-    inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-    outputs = { self, nixpkgs }: let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-        devShells.${system}.default = pkgs.mkShell {
-            packages = with pkgs; [ bun ];
-            shellHook = ''
-                export PS1="\w $ ";
-                cd app && bun run dev
-            '';
-        };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
     };
+
+    outputs = { nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let pkgs = nixpkgs.legacyPackages.${system}; in {
+                devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [ bun ];
+                };
+            }
+        );
 }
